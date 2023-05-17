0bdcfe345e6b2a64f34d411d323ecc1859ea23e8
James <ptrsnja@gmail.com>
2023-05-17 13:58:02 -0400

    basic input access prompt and status

--- packages/audio/src/lib/audio-input/audio-input.service.ts
+++ packages/audio/src/lib/audio-input/audio-input.service.ts
@@ -1,22 +1,42 @@
-import { Injectable, Signal, signal } from '@angular/core';
+import { Injectable, Signal, WritableSignal, computed, signal } from '@angular/core';
+import { Observable, from } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AudioInputService {
 
-  public selectedInput: Signal<string>;
+  private _deviceInfoList: WritableSignal<MediaDeviceInfo[]>;
   constructor() { 
     console.log('audio input service constructor');
-    this.selectedInput = signal('');
+    this._deviceInfoList = signal<MediaDeviceInfo[]>([]);
+    this.listAudioDevices();
   }
 
   public listAudioDevices(): Signal<MediaDeviceInfo[]> {
-    const list = signal<MediaDeviceInfo[]>([]);
-    navigator.mediaDevices.enumerateDevices().then((info: MediaDeviceInfo[]) => {
-      console.log('got media device info list', info);
-      list.set(info);
-    });
-    return list;
+    console.log('list audio devices')
+    if (!this._deviceInfoList()?.length) {
+      navigator.mediaDevices.enumerateDevices().then((info: MediaDeviceInfo[]) => {
+        this._deviceInfoList.set(info);
+      });
+    }
+    return this._deviceInfoList;
+  }
+
+  public hasAudioPermission(opt: { input?: boolean, output?: boolean}): Signal<boolean> {
+    const perm = computed(() => this._deviceInfoList().some((info) => {
+      if (opt.input && info.kind === 'audioinput') {
+        return !!info.deviceId;
+      }
+      if (opt.output && info.kind === 'audiooutput') {
+        return !!info.deviceId;
+      }
+      return ((info.kind === 'audioinput' || info.kind === 'audiooutput') && !!info.deviceId)
+    }))
+    return perm;
+  }
+
+  public getAudioInputStream(): Observable<MediaStream> {
+    return from(navigator.mediaDevices.getUserMedia({video: false, audio: true}));
   }
 }
--- packages/client/src/app/input-device/input-device.component.ts
+++ packages/client/src/app/input-device/input-device.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
-import { Component, Signal, computed } from '@angular/core';
+import { Component, OnDestroy, OnInit, Signal, WritableSignal, computed, signal } from '@angular/core';
 import { AudioInputService, AudioModule } from '@captionator/audio';
+import { Subject, catchError, of, take } from 'rxjs';
 
 @Component({
   selector: 'captionator-input-device',
@@ -12,10 +13,40 @@ import { AudioInputService, AudioModule } from '@captionator/audio';
   templateUrl: './input-device.component.html',
   styleUrls: ['./input-device.component.scss']
 })
-export class InputDeviceComponent {
-  public selectedInput: Signal<string>;
+
+export class InputDeviceComponent implements OnInit, OnDestroy {
+  public inputState: WritableSignal<string | null>;
+  public inputs: Signal<MediaDeviceInfo[]>;
+  public access: Signal<boolean>;
+
+  private onDestroy$: Subject<void> = new Subject<void>();
   constructor(private inputService: AudioInputService) {
-    this.selectedInput = computed(() => this.inputService.selectedInput());
-    console.log('input device component constructor', this.selectedInput());
+    this.inputState = signal<string | null>(null);
+    const inputList: Signal<MediaDeviceInfo[]> = this.inputService.listAudioDevices();
+    // this.inputs  = this.inputService.listAudioDevices();
+    this.inputs = computed(() => inputList().filter((info) => (info.kind === 'audioinput' && info.deviceId !== '')));
+    this.access = this.inputService.hasAudioPermission({input: true});
+  }
+  
+  ngOnInit(): void {
+    console.log('init');
+    // this.streamAudio();
+  }
+
+  ngOnDestroy(): void {
+    this.onDestroy$.next();
+  }
+
+  streamAudio() {
+    this.inputService.getAudioInputStream().pipe(
+      take(1),
+      catchError((err) => {
+        console.log(err.message);
+        this.inputState.set(`Error: ${err.message}`);
+        return of(err);
+      })
+    ).subscribe((stream) => {
+      console.log('stream', stream);
+    })
   }
 }
